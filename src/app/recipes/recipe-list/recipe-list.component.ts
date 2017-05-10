7e613c5d430534f983137a6a0a61ee7d83bc1780
jmdagenais <jmdagenais@acquisio.com>
2017-05-10 12:19:41 -0400

    finish components template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
     moduleId: module.id,
-    selector: 'recipe-list',
-    templateUrl: 'recipe-list.component.html'
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-    constructor() { }
+  recipes: Array<Recipe> = [
+    new Recipe('test recipe', 'a test with a pinch of salt',
+      'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+    new Recipe('Pouding au chocolat', 'pouding au choco à la mijoteuse',
+      'https://images.ricardocuisine.com/services/recipes/260x351_13783861245477d88bd6b72.jpg')
+  ];
+  constructor() { }
 
-    ngOnInit() { }
+  ngOnInit() { }
 
 }
